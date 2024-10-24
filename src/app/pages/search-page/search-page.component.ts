f9bd96d540d1f7a2fb5365b72fcad047e27214be
Kevin <kevin005005@gmail.com>
2024-10-24 15:21:12 +0200

    Search is working now

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,24 +1,37 @@
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
+import { Product } from '../../DTOs/Product';
+import { ProductGridComponent } from '../../sections/product-grid/product-grid.component';
+import { ProductService } from '../../services/product.service';
 
 @Component({
     selector: 'app-search-page',
     standalone: true,
-    imports: [AsyncPipe, NgIf],
+    imports: [AsyncPipe, NgIf, ProductGridComponent],
     templateUrl: './search-page.component.html',
     styleUrl: './search-page.component.css'
 })
 export class SearchPageComponent {
     keyword$: Observable<string>;
+    products$: Observable<Product[]>;
+    products: Product[] = [];
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private productService: ProductService) {
         this.keyword$ = this.route.queryParamMap.pipe(
             map(params => {
                 console.log('Query params: ', params);
                 return params.get('q') || '';
             })
         );
+
+        this.products$ = this.keyword$.pipe(
+            switchMap(keyword => this.productService.getProductsByKeyword(keyword))
+        );
+
+        this.products$.subscribe(products => {
+            this.products = products;
+        });
     }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { ApiClientService } from './api-client.service';
 import { Product } from '../DTOs/Product';
 import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService extends ApiClientService {
-
     public getProductById(publicId: string): Observable<Product> {
-        return this.http.get<Product>(`${this.BASE_URL}/product/${publicId}`);
+        return this.http.get<Product>(`${this.BASE_URL}/products/${publicId}`);
+    }
+    public getProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(`${this.BASE_URL}/products`);
+    }
+    public getProductsByKeyword(keyword: string): Observable<Product[]> {
+        const params = new HttpParams({ fromObject: { keyword: keyword } });
+        return this.http.get<Product[]>(`${this.BASE_URL}/products`, { params: params });
     }
 }
