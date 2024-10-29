e183b7a8702be478f8d0f681925132d4e598f536
Kevin <kevin005005@gmail.com>
2024-10-29 01:39:38 +0100

    Ugly but working solution

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Image } from '../DTOs/Image';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ImageService {
     readonly S3_PATH = 'https://ecommerce420.s3.eu-central-1.amazonaws.com/'
+    readonly BLANK = this.getImageUrl('blank');
+    readonly BLANK_IMAGE = new Image(0, 0, 'blank');
 
     constructor() { }
 
